46a986debc2669f4538880ae5b979a29b56c0f3d
mdyasin7636 <mdyasin7636@gmail.com>
2024-05-29 18:28:23 +0600

    about section updated

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -5,16 +5,16 @@ const DashboardLayout = () => {
     <div className="grid grid-cols-12">
       <div className="col-span-2 bg-gray-300 min-h-screen p-12">
         <ul>
-          <li className="border border-black p-4 text w-full">
+          <li className="font-semibold text-lg p-4 w-full">
             <Link to={"home"}>Dashboard</Link>
           </li>
-          <li className="border border-black p-4 text w-full">
+          <li className="font-semibold text-lg p-4 w-full">
             <Link to={"all-products"}>All Products</Link>
           </li>
-          <li className="border border-black p-4 text w-full">
+          <li className="font-semibold text-lg p-4 w-full">
             <Link to={"add-products"}>Add Product</Link>
           </li>
-          <li className="border border-black p-4 text w-full">
+          <li className="font-semibold text-lg p-4 w-full">
             <Link to={"/"}>Home</Link>
           </li>
         </ul>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,32 @@
-
+/* eslint-disable react/no-unescaped-entities */
 const About = () => {
-    
-    return (
-        <div>
-            <h1>This is About</h1>
-        </div>
-    );
+  return (
+    <div className="px-20 py-36 font-semibold">
+      <h1>
+        Welcome to Prime, your ultimate destination for high-quality sports
+        shoes. At Prime, we are passionate about helping athletes and fitness
+        enthusiasts achieve their goals by providing top-notch footwear that
+        combines performance, comfort, and style.
+      </h1>
+      <br />
+      <h1>
+        Our journey began with a simple mission: to offer a diverse selection of
+        sports shoes that cater to various athletic needs, whether you're a
+        professional athlete or a casual runner. We carefully curate our
+        collection from trusted brands and ensure each pair meets our high
+        standards of quality and durability.
+      </h1>
+      <br />
+      <h1>
+        What sets Prime apart is our commitment to customer satisfaction. We
+        strive to create a seamless shopping experience with detailed product
+        descriptions, a user-friendly interface, and exceptional customer
+        service. Our team is always ready to assist you in finding the perfect
+        pair of shoes to elevate your game. Join the Prime community today and
+        step into excellence with every stride.
+      </h1>
+    </div>
+  );
 };
 
 export default About;
